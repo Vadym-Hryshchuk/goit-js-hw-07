96136cbd58dbe6f7dc6909bfb6f25ec15e69ea89
Vadym-Hryshchuk <82964921+Vadym-Hryshchuk@users.noreply.github.com>
2021-09-30 19:25:01 +0300

    Домашня робота № 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
+textEl.setAttribute('style', `font-size: ${inputEl.value}px`);
 
 const functions = () => {
   textEl.setAttribute('style', `font-size: ${inputEl.value}px`);
--- js/task-08.js
+++ js/task-08.js
@@ -12,18 +12,20 @@ const clickHendler = () => {
     
     createBoxes(amount)
 };
-
+let defaultSize = 20;
 const createBoxes = (amount) => {
     const markupDiv = [];
-    const defaultSize = 30;
+    // const defaultSize = 30;
     
     for (let i = 0; i < amount; i += 1) {
         const colorR = Math.floor(Math.random() * 256);
         const colorG = Math.floor(Math.random() * 256);
         const colorB = Math.floor(Math.random() * 256);
-        const sizeDiv = defaultSize + i * 10;
+        defaultSize += 10;
+        // const sizeDiv = defaultSize + i * 10;
         const divEl = document.createElement("div");
-        divEl.setAttribute('style', `width: ${sizeDiv}px; height: ${sizeDiv}px; background-color: rgb(${colorR},${colorG},${colorB})`);
+        divEl.setAttribute('style', `width: ${defaultSize}px; height: ${defaultSize}px; background-color: rgb(${colorR},${colorG},${colorB})`);
+        boxesElement.value = "";
         markupDiv.push(divEl);
         
     }
@@ -34,6 +36,7 @@ const renderBtnClickHendler = renderBtn.addEventListener("click", clickHendler)
 
 const destroyBoxes = () => {
     boxesElements.innerHTML = "";
-    boxesElement.value = ""
+    boxesElement.value = "";
+     defaultSize = 20;
 };
 const destroyBtnClickHendler = destroyBtn.addEventListener("click", destroyBoxes);
